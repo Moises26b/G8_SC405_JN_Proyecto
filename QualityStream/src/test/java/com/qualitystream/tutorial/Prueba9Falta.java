06dd3ba4c6ed59b1ba88a5de4fb46b5997f8b1b8
moises26b <Moisesbca26@gmail.com>
2022-12-05 15:26:44 -0600

    Prueba 9

--- QualityStream/src/test/java/com/qualitystream/tutorial/Prueba9Falta.java
+++ QualityStream/src/test/java/com/qualitystream/tutorial/Prueba9Falta.java
@@ -33,40 +33,7 @@ public class Prueba9Falta {
     	
     	WebElement Fb = driver.findElement(By.className("a2a_button_facebook"));
     	Fb.click();
-    	//driver.switchTo().window("Facebook");
-    	//String winHandleBefore = driver.getWindowHandle();
-
-    	// Perform the click operation that opens new window
-
-    	//Switch to new window opened
-    	//for(String winHandle : driver.getWindowHandles()){
-    	   // driver..window("Facebook");
-    	//WebElement face = driver.findElement(By.className("a2a_svg a2a_s__default a2a_s_facebook"));
-    	/*Alert a = driver.switchTo().alert();
-    	driver.switchTo ().alert();
-    	a.accept();*/
     	
-
-        
-               
-    	//driver.get("https://www.addtoany.com/add_to/facebook?linkurl=https%3A%2F%2Fsistemas.ufidelitas.ac.cr%2Fnokri-demo2%2Fsignin%2F&linkname=Signin&linknote=");
-    	/*WebElement email = driver.findElement(By.id("email"));
-    	email.click();
-    	email.sendKeys("Mooibca@gmail.com");
-    	email.submit();
-    	
-    	WebElement pass = driver.findElement(By.id("pass"));
-    	pass.sendKeys("MOI12345");
-    	
-    	// Switch back to original browser (first window)
-    	//driver.switchTo().window(winHandleBefore); 
-    	
-    	pass.submit();
-    	/*WebElement List = driver.findElement(By.className("select2-results__option"));
-    	
-    	List.click();
-    	
-    	Lista.submit();*/
     	
     	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     	
